c33eb29842c6dbf36d1840af6d708599a3902fe1
Ram <ramgunti888@gmail.com>
2026-08-08 22:17:26 +0530

    Time: 21 ms (89.34%) | Memory: 98.1 MB (6.61%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -1,13 +1,24 @@
 class Solution {
 public:
-    int findKthLargest(vector<int>& nums, int k) {
-        priority_queue<int, vector<int>, greater<int>> pq;
-
-        for(int i=0; i<nums.size(); i++){
-            pq.push(nums[i]);
-            if(pq.size()>k) pq.pop();
+    int quick_select(vector<int> arr, int k){
+        vector<int> left;
+        vector<int> right;
+        vector<int> mid;
+        
+        int pivot = arr[rand()%arr.size()];
+        
+        for(int i=0; i<arr.size(); i++){
+            if(arr[i]>pivot) left.push_back(arr[i]);
+            else if(arr[i]<pivot) right.push_back(arr[i]);
+            else mid.push_back(arr[i]);
         }
+        
+        if(left.size()>=k) return quick_select(left, k);
+        else if(left.size()+mid.size()<k) return quick_select(right, k-left.size()-mid.size());
+        else return pivot;
+    }
 
-        return pq.top();
+    int findKthLargest(vector<int>& nums, int k) {
+        return quick_select(nums, k);
     }
 };
