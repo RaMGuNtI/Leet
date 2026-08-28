5b7b841ff942e508cbd3fbe8710482d9a455f6bd
Ram <ramgunti888@gmail.com>
2026-08-28 15:14:33 +0530

    Time: 195 ms (66.91%) | Memory: 25.9 MB (56.99%) - LeetSync

--- 2605-count-anagrams/count-anagrams.py
+++ 2605-count-anagrams/count-anagrams.py
@@ -1,20 +1,30 @@
-import math
 from collections import Counter
 
 class Solution:
     def countAnagrams(self, s: str) -> int:
-        words = s.split()
-        ans = 1
         MOD = 10**9 + 7
+        n = len(s)
 
-        for word in words:
-            num = math.factorial(len(word))
+        # Precompute factorials
+        fact = [1] * (n + 1)
+
+        for i in range(1, n + 1):
+            fact[i] = (fact[i - 1] * i) % MOD
+        print(fact)
+
+        ans = 1
+
+        for word in s.split():
+            # numerator
+            ways = fact[len(word)]
+
+            # denominator
             freq = Counter(word)
 
-            den = 1
             for count in freq.values():
-                den *= math.factorial(count)
+                ways = ways * pow(fact[count], MOD - 2, MOD)
+                ways %= MOD
 
-            ans = (ans * (num // den)) % MOD
+            ans = (ans * ways) % MOD
 
         return ans
