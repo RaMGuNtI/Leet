26d37cd7a18f294126266dd0951afc528301941b
Ram <ramgunti888@gmail.com>
2026-09-03 10:24:14 +0530

    Time: 95 ms (5.50%) | Memory: 183 MB (6.54%) - LeetSync

--- 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.cpp
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.cpp
@@ -1,27 +1,18 @@
 class Solution {
 public:
     bool uniformArray(vector<int>& nums1) {
-        bool isEven = true;
-        bool isOdd = true;
-        
         priority_queue<int, vector<int>, greater<int>> evp;
         priority_queue<int, vector<int>, greater<int>> odp;
 
         for(int i: nums1){
             if(i%2==0){
-                isOdd = false;
                 evp.push(i);
-            }
-        }
-
-        for(int i: nums1){
-            if(i%2!=0){
-                isEven = false;
+            }else{
                 odp.push(i);
             }
         }
 
-        if(isOdd || isEven) return true;
+        if(evp.empty() || odp.empty()) return true;
 
         while(!odp.empty() && !evp.empty()){
             if(evp.top()>odp.top()) evp.pop();
