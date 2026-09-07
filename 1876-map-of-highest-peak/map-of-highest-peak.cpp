70b2d1723fbb4f9ceee1e7ef7c749f0a7277ec9b
Ram <ramgunti888@gmail.com>
2026-09-07 22:45:17 +0530

    Time: 75 ms (68.18%) | Memory: 112.9 MB (75.83%) - LeetSync

---
+++ 1876-map-of-highest-peak/map-of-highest-peak.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    vector<vector<int>> highestPeak(vector<vector<int>>& isWater) {
+        int m = isWater.size();
+        int n = isWater[0].size();
+
+        vector<pair<int, int>> directions = {
+            {0, 1}, {1, 0}, {0, -1}, {-1, 0}
+        };
+
+        vector<vector<int>> ans(m, vector<int>(n, -1));
+
+        queue<pair<int, int>> q;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+
+                if (isWater[i][j] == 1) {
+                    q.push({i, j});
+                    ans[i][j] = 0;
+                }
+            }
+        }
+
+        while (!q.empty()) {
+
+            auto [row, col] = q.front();
+            q.pop();
+
+            for (auto dir : directions) {
+
+                int nrow = row + dir.first;
+                int ncol = col + dir.second;
+
+                if (nrow < 0 || nrow >= m ||
+                    ncol < 0 || ncol >= n) {
+                    continue;
+                }
+
+                if (ans[nrow][ncol] != -1) {
+                    continue;
+                }
+
+                ans[nrow][ncol] = ans[row][col] + 1;
+
+                q.push({nrow, ncol});
+            }
+        }
+
+        return ans;
+    }
+};
