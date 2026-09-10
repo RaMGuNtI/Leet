fd244a776f7f5c73cc229867f38707f6abfc849a
Ram <ramgunti888@gmail.com>
2026-09-10 10:01:00 +0530

    Time: 3 ms (91.06%) | Memory: 15.6 MB (84.80%) - LeetSync

--- 2347-count-nodes-equal-to-average-of-subtree/count-nodes-equal-to-average-of-subtree.cpp
+++ 2347-count-nodes-equal-to-average-of-subtree/count-nodes-equal-to-average-of-subtree.cpp
@@ -11,48 +11,23 @@
  */
 class Solution {
 public:
+    int ans = 0;
+    pair<int, int> dfs(TreeNode* root){
+        if(!root) return {0, 0};
+
+        auto left = dfs(root->left);
+        auto right = dfs(root->right);
+
+        int sumo = left.first + right.first + root->val;
+        int count = left.second + right.second + 1;
+
+        if(sumo/count==root->val) ans++;
+
+        return {sumo, count};
+    }
+
     int averageOfSubtree(TreeNode* root) {
-        queue<TreeNode*> q;
-        vector<TreeNode*> add;
-        add.push_back(root);
-        q.push(root);
-        while(!q.empty()){
-            TreeNode* tp = q.front();
-            q.pop();
-            if(tp->right){
-                q.push(tp->right);
-                add.push_back(tp->right);
-            }
-            if(tp->left){
-                q.push(tp->left);
-                add.push_back(tp->left);
-            }
-        }
-        int ans = 0;
-
-        for(int i=0; i<add.size(); i++){
-            queue<TreeNode*> nq;
-            nq.push(add[i]);
-            int sumo =0;
-            int numCount = 0;
-            while(!nq.empty()){
-                TreeNode* tp = nq.front();
-                numCount++;
-                sumo+=tp->val;
-                nq.pop();
-                if(tp->right){
-                    nq.push(tp->right);
-                }
-                if(tp->left){
-                    nq.push(tp->left);
-                }
-            }
-            cout<<"sumo: "<<sumo<<", numCount: "<<numCount<<endl;
-            if(floor(sumo/numCount)==add[i]->val){
-                ans++;
-            }
-        }
-
+        dfs(root);
         return ans;
     }
 };
