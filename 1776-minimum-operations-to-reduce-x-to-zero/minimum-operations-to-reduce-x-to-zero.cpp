06f3960e135c9ee77850ddcde8042eaeb5530a15
Ram <ramgunti888@gmail.com>
2026-09-23 16:01:58 +0530

    Time: 5 ms (48.74%) | Memory: 102.4 MB (38.48%) - LeetSync

--- 1776-minimum-operations-to-reduce-x-to-zero/minimum-operations-to-reduce-x-to-zero.cpp
+++ 1776-minimum-operations-to-reduce-x-to-zero/minimum-operations-to-reduce-x-to-zero.cpp
@@ -1,39 +1,39 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums, int x) {
+        int tsum = accumulate(nums.begin(), nums.end(), 0);
         const int n = nums.size();
 
-        vector<int> pre(n), suff(n);
-        unordered_map<int, int> mp;
 
-        pre[0] = nums[0];
-        mp[pre[0]] = 0;
+        if(tsum == x) return n;
+        int target = tsum - x;
 
-        for(int i = 1; i < n; i++){
-            pre[i] = pre[i - 1] + nums[i];
-            mp[pre[i]] = i;
-        }
+        int i = 0, j = 0, sum = 0, mx = 0;
 
-        suff[n - 1] = nums[n - 1];
-        for(int i = n - 2; i >= 0; i--){
-            suff[i] = suff[i + 1] + nums[i];
-        }
+        while (j < n) { 
+            sum += nums[j];
 
-        int mini = INT_MAX;
-        for (int i = 0; i < n; i++){
-            if (pre[i] == x) mini = min(mini, i + 1);
-            if (suff[i] == x) mini = min(mini, n - i);
-        }
+            if (sum == target){
+                mx = max(mx, j - i + 1);
+                j++;
 
+            } else if(sum > target) {
+                while (i <= j and sum > target) {
+                    sum -= nums[i];
+                    i++;
+                }
 
-        for (int i = n - 1; i >= 0; i--){
-            int diff = x - suff[i];
+              
+                if (sum == target) mx = max(mx, j - i + 1);
+                 j++;
 
-            if (mp.count(diff) and mp[diff] + 1 < i) {
-                mini = min(mini, (n - i) + mp[diff] + 1);
+            } else {
+                j++;
             }
         }
+        cout << mx << endl;
 
-        return (mini == INT_MAX ? -1 : mini);
+        int res = n - mx;
+        return (mx == 0 ? -1 : res);
     }
 };
